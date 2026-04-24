324325d17fca258224142ab871efc8a78c3b7c43
anishdass <anishdassatoffice@gmail.com>
2026-04-24 17:15:12 +0100

    feat: add JPA persistence and UserRepository
    Configured the User class as a persistent entity and implemented the UserRepository to handle database interactions via Spring Data JPA.

--- src/main/java/com/app/ecomm_application/User.java
+++ src/main/java/com/app/ecomm_application/User.java
@@ -1,10 +1,21 @@
 package com.app.ecomm_application;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private Long id;
     private String firstName;
     private String lastName;
+
+
 }
