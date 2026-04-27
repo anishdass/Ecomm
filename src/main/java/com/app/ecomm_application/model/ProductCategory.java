9885fcde49177804c18610a1f44573af3a7108de
anishdass <anishdassatoffice@gmail.com>
2026-04-27 15:50:34 +0100

    update: product entity and repository created

---
+++ src/main/java/com/app/ecomm_application/model/ProductCategory.java
@@ -0,0 +1,5 @@
+package com.app.ecomm_application.model;
+
+public enum ProductCategory {
+
+}
---
+++ src/main/java/com/app/ecomm_application/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.app.ecomm_application.repository;
+
+import com.app.ecomm_application.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
