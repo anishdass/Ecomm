c2cb6b9bd5000140c990e59c5fed0010e797d954
anishdass <anishdassatoffice@gmail.com>
2026-04-27 21:19:28 +0100

    update: CRUD operations for Product in place

---
+++ src/main/java/com/app/ecomm_application/controller/ProductController.java
@@ -0,0 +1,31 @@
+package com.app.ecomm_application.controller;
+
+import com.app.ecomm_application.dto.ProductRequest;
+import com.app.ecomm_application.dto.ProductResponse;
+import com.app.ecomm_application.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@RequestBody ProductRequest productRequest){
+        return new ResponseEntity<>(productService.createProduct(productRequest), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable Long id, @RequestBody ProductRequest productRequest){
+        return productService.updateProduct(id, productRequest)
+                .map(ResponseEntity::ok)
+                .orElseGet(()->ResponseEntity
+                        .notFound()
+                        .build());
+    }
+}
